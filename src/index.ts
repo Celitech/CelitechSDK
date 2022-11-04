be4e2d295daf90d5d7dc2a76119dc73f2eaa209c
Michel Haddad <michel@celitech.com>
2022-11-04 16:27:50 +0200

    basic setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export class Celitech {
+  public readonly purchases;
+  public readonly destinations;
+  public readonly packages;
+
+  public constructor(clientId, clientSecret) {
+    console.log(clientId, clientSecret);
+  }
+}
