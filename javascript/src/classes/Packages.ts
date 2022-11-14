2ed2d4f4af7e1da11973667b977736c35ad8214f
Michel Haddad <michel@celitech.com>
2022-11-14 18:12:27 +0200

    add packages class

---
+++ javascript/src/classes/Packages.ts
@@ -0,0 +1,46 @@
+import { AxiosInstance } from "axios";
+
+interface PackagesListRequest {
+  destination?: string;
+  startTime?: number;
+  endtime?: number;
+  duration?: number;
+  afterCursor?: string;
+  limit?: number;
+}
+
+interface Package {
+  id: string;
+  destination: string;
+  dataLimitInBytes: number;
+  minDays: number;
+  maxDays: number;
+  priceInCents: number;
+}
+
+interface PackagesListResponse {
+  packages: Package[];
+  afterCursor: string;
+}
+
+export class Packages {
+  private axiosInstance;
+
+  public constructor(axiosInstance: AxiosInstance) {
+    this.axiosInstance = axiosInstance;
+  }
+
+  public async list(
+    request?: PackagesListRequest
+  ): Promise<PackagesListResponse> {
+    if (request) {
+      const response = await this.axiosInstance.get("/packages", {
+        params: request,
+      });
+      return response.data;
+    }
+
+    const response = await this.axiosInstance.get("/packages");
+    return response.data;
+  }
+}
--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,14 +1,20 @@
 import { getAxiosInstance } from "./axios/axios";
 import { Destinations } from "./classes/Destinations";
+import { Packages } from "./classes/Packages";
 
+interface Credentials {
+  clientId: string;
+  clientSecret: string;
+}
 export class Celitech {
   public readonly destinations;
   //public readonly purchases;
-  //public readonly packages;
+  public readonly packages;
 
-  public constructor(clientId: string, clientSecret: string) {
+  public constructor({ clientId, clientSecret }: Credentials) {
     const axiosInstance = getAxiosInstance(clientId, clientSecret);
 
     this.destinations = new Destinations(axiosInstance);
+    this.packages = new Packages(axiosInstance);
   }
 }
