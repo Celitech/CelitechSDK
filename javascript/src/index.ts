afd82a196af8ba6c092abc6e7acc9ecb8a71dce3
Michel Haddad <michel@celitech.com>
2022-11-15 15:14:28 +0200

    Setup purchase class

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,14 +1,21 @@
 import { getAxiosInstance } from "./axios/axios";
 import { Destinations } from "./classes/Destinations";
+import { Purchases } from "./classes/Purchases";
+
+interface Credentials {
+  clientId: string;
+  clientSecret: string;
+}
 
 export class Celitech {
   public readonly destinations;
-  //public readonly purchases;
+  public readonly purchases;
   //public readonly packages;
 
-  public constructor(clientId: string, clientSecret: string) {
+  public constructor({ clientId, clientSecret }: Credentials) {
     const axiosInstance = getAxiosInstance(clientId, clientSecret);
 
     this.destinations = new Destinations(axiosInstance);
+    this.purchases = new Purchases(axiosInstance);
   }
 }
