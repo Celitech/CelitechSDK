2b3d3f0722c6a2ec593e6f10dfec8167ff45f0e9
Michel Haddad <michel@celitech.com>
2022-11-15 20:53:21 +0200

    Complete the purchases class

--- javascript/src/classes/Purchases.ts
+++ javascript/src/classes/Purchases.ts
@@ -22,6 +22,60 @@ interface CreatePurchaseResponse {
   };
 }
 
+interface EditPurchaseRequest {
+  purchaseId: string;
+  startTime: number;
+  endTime: number;
+}
+
+interface EditPurchaseResponse {
+  purchaseId: string;
+  newStartTime: number;
+  newEndTime: number;
+}
+
+interface ListPurchasesRequest {
+  customerId?: string;
+  afterCursor?: string;
+  limit?: number;
+  before?: number;
+  after?: number;
+}
+
+interface Purchase {
+  id: string;
+  anonymous: boolean;
+  startTime: number;
+  endTime: number;
+  createdAt: number;
+  customer: {
+    id: string;
+  };
+  package: {
+    id: string;
+    dataLimitInBytes: number;
+    destination: string;
+    priceInCents: number;
+  };
+  esim: {
+    iccid: string;
+  };
+}
+
+interface ListPurchasesResponse {
+  purchases: Purchase[];
+  afterCursor: string;
+}
+
+interface CheckConsumptionRequest {
+  purchaseId: string;
+}
+
+interface CheckConsumptionResponse {
+  dataUsageRemainingInBytes: number;
+  status: string;
+}
+
 export class Purchases {
   private axiosInstance;
 
@@ -33,6 +87,37 @@ export class Purchases {
     request: CreatePurchaseRequest
   ): Promise<CreatePurchaseResponse> {
     const response = await this.axiosInstance.post("/purchases", request);
+    return response.data;
+  }
+
+  public async edit(
+    request: EditPurchaseRequest
+  ): Promise<EditPurchaseResponse> {
+    const response = await this.axiosInstance.post("/purchases/edit", request);
+    return response.data;
+  }
+
+  public async list(
+    request?: ListPurchasesRequest
+  ): Promise<ListPurchasesResponse> {
+    if (request) {
+      const response = await this.axiosInstance.get("/purchases", {
+        params: request,
+      });
+      return response.data;
+    }
+
+    const response = await this.axiosInstance.get("/purchases");
+    return response.data;
+  }
+
+  public async checkConsumption(
+    request: CheckConsumptionRequest
+  ): Promise<CheckConsumptionResponse> {
+    const response = await this.axiosInstance.get(
+      `/purchases/${request.purchaseId}/consumption`
+    );
+    console.log("here =>", request.purchaseId);
     return response.data;
   }
 }
