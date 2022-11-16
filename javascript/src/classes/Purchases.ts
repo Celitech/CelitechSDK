efce30881715b2684439f6522984fb3d0f941430
michelhdd <112699756+michelhdd@users.noreply.github.com>
2022-11-16 15:54:06 +0200

    Add purchase class (#3)

---
+++ javascript/src/classes/Purchases.ts
@@ -0,0 +1,122 @@
+import { AxiosInstance } from "axios";
+
+interface CreatePurchaseRequest {
+  destination: string;
+  dataLimitInGB: number;
+  startTime: number;
+  endTime: number;
+  customerId?: number;
+  sessionId?: number;
+}
+
+interface CreatePurchaseResponse {
+  purchase: {
+    id: string;
+    packageId: string;
+    startTime: number;
+    endTime: number;
+  };
+  profile: {
+    iccid: string;
+    activationCode: string;
+  };
+}
+
+interface EditPurchaseRequest {
+  purchaseId: string;
+  startTime: number;
+  endTime: number;
+}
+
+interface EditPurchaseResponse {
+  purchaseId: string;
+  newStartTime: number;
+  newEndTime: number;
+}
+
+interface ListPurchasesRequest {
+  customerId?: string;
+  afterCursor?: string;
+  limit?: number;
+  before?: number;
+  after?: number;
+}
+
+interface Purchase {
+  id: string;
+  anonymous: boolean;
+  startTime: number;
+  endTime: number;
+  createdAt: number;
+  customer: {
+    id: string;
+  };
+  package: {
+    id: string;
+    dataLimitInBytes: number;
+    destination: string;
+    priceInCents: number;
+  };
+  esim: {
+    iccid: string;
+  };
+}
+
+interface ListPurchasesResponse {
+  purchases: Purchase[];
+  afterCursor: string;
+}
+
+interface CheckConsumptionRequest {
+  purchaseId: string;
+}
+
+interface CheckConsumptionResponse {
+  dataUsageRemainingInBytes: number;
+  status: string;
+}
+
+export class Purchases {
+  private axiosInstance;
+
+  public constructor(axiosInstance: AxiosInstance) {
+    this.axiosInstance = axiosInstance;
+  }
+
+  public async create(
+    request: CreatePurchaseRequest
+  ): Promise<CreatePurchaseResponse> {
+    const response = await this.axiosInstance.post("/purchases", request);
+    return response.data;
+  }
+
+  public async edit(
+    request: EditPurchaseRequest
+  ): Promise<EditPurchaseResponse> {
+    const response = await this.axiosInstance.post("/purchases/edit", request);
+    return response.data;
+  }
+
+  public async list(
+    request?: ListPurchasesRequest
+  ): Promise<ListPurchasesResponse> {
+    if (request) {
+      const response = await this.axiosInstance.get("/purchases", {
+        params: request,
+      });
+      return response.data;
+    }
+
+    const response = await this.axiosInstance.get("/purchases");
+    return response.data;
+  }
+
+  public async checkConsumption(
+    request: CheckConsumptionRequest
+  ): Promise<CheckConsumptionResponse> {
+    const response = await this.axiosInstance.get(
+      `/purchases/${request.purchaseId}/consumption`
+    );
+    return response.data;
+  }
+}
--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,6 +1,12 @@
 import { getAxiosInstance } from "./axios/axios";
 import { Destinations } from "./classes/Destinations";
+import { Purchases } from "./classes/Purchases";
 import { Packages } from "./classes/Packages";
+
+interface Credentials {
+  clientId: string;
+  clientSecret: string;
+}
 
 interface Credentials {
   clientId: string;
@@ -8,13 +14,14 @@ interface Credentials {
 }
 export class Celitech {
   public readonly destinations;
-  //public readonly purchases;
+  public readonly purchases;
   public readonly packages;
 
   public constructor({ clientId, clientSecret }: Credentials) {
     const axiosInstance = getAxiosInstance(clientId, clientSecret);
 
     this.destinations = new Destinations(axiosInstance);
+    this.purchases = new Purchases(axiosInstance);
     this.packages = new Packages(axiosInstance);
   }
 }
