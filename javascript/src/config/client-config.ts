8910d751845aa91a11f9857d040e3f64de44fd27
Michel Haddad <michel@celitech.com>
2022-11-17 15:48:20 +0200

    update client config

--- javascript/src/config/client-config.ts
+++ javascript/src/config/client-config.ts
@@ -1,10 +1,3 @@
 export const OAUTH2_URL = process.env.OAUTH2_URL;
 
 export const API_URL = process.env.API_URL;
-
-const account_auth_data = {
-  username: process.env.CLIENT_ID!,
-  password: process.env.CLIENT_SECRET!,
-};
-
-export const AUTH_DATA = account_auth_data;
