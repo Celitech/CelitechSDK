a0607a933ef4853baa769be755a1b025f03cbec2
Michel Haddad <michel@celitech.com>
2022-11-25 18:40:48 +0200

    Update generate release it script

--- javascript/scripts/generate-release-it-config.ts
+++ javascript/scripts/generate-release-it-config.ts
@@ -1,67 +1,54 @@
-// {
-//     "hooks": {},
-//     "git": {
-//       "changelog": "git log --pretty=format:\"* %s (%h)\" ${from}...${to}",
-//       "requireCleanWorkingDir": true,
-//       "requireBranch": false,
-//       "requireUpstream": true,
-//       "requireCommits": false,
-//       "addUntrackedFiles": false,
-//       "commit": true,
-//       "commitMessage": "Release ${version}",
-//       "commitArgs": [],
-//       "tag": true,
-//       "tagName": null,
-//       "tagMatch": null,
-//       "tagAnnotation": "Release ${version}",
-//       "tagArgs": [],
-//       "push": true,
-//       "pushArgs": ["--follow-tags"],
-//       "pushRepo": ""
-//     },
-//     "npm": {
-//       "publish": true,
-//       "publishPath": ".",
-//       "publishArgs": [],
-//       "tag": null,
-//       "otp": null,
-//       "ignoreVersion": false,
-//       "allowSameVersion": false,
-//       "versionArgs": [],
-//       "skipChecks": false,
-//       "timeout": 10
-//     },
-//     "github": {
-//       "release": false,
-//       "releaseName": "Release ${version}",
-//       "releaseNotes": null,
-//       "autoGenerate": false,
-//       "preRelease": false,
-//       "draft": false,
-//       "tokenRef": "GITHUB_TOKEN",
-//       "assets": null,
-//       "host": null,
-//       "timeout": 0,
-//       "proxy": null,
-//       "skipChecks": false,
-//       "web": false
-//     }
-//   }
-
 import * as fs from "fs";
 
-let GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
+let GITHUB_TOKEN = process.env["GITHUB_TOKEN"] || "";
 
 const codeToGenerate = `{
     "hooks": {},
-    "npm":{
+    "git": {
+        "changelog": "git log --pretty=format:\\"* %s (%h)\\" \${from}...\${to}",
+        "requireCleanWorkingDir": true,
+        "requireBranch": false,
+        "requireUpstream": true,
+        "requireCommits": false,
+        "addUntrackedFiles": false,
+        "commit": true,
+        "commitMessage": "Release \${version}",
+        "commitArgs": [],
+        "tag": true,
+        "tagName": null,
+        "tagMatch": null,
+        "tagAnnotation": "Release \${version}",
+        "tagArgs": [],
+        "push": true,
+        "pushArgs": ["--follow-tags"],
+        "pushRepo": ""
+    },
+    "npm": {
+        "publish": true,
+        "publishPath": ".",
+        "publishArgs": [],
+        "tag": null,
+        "otp": null,
         "ignoreVersion": false,
-        "allowSameVersion": false
+        "allowSameVersion": false,
+        "versionArgs": [],
+        "skipChecks": false,
+        "timeout": 10
     },
     "github": {
-        "tokenRef": "${GITHUB_TOKEN}",
         "release": true,
-        "autoGenerate": true
+        "releaseName": "Release \${version}",
+        "releaseNotes": null,
+        "autoGenerate": true,
+        "preRelease": false,
+        "draft": false,
+        "tokenRef": "${GITHUB_TOKEN}",
+        "assets": null,
+        "host": null,
+        "timeout": 0,
+        "proxy": null,
+        "skipChecks": false,
+        "web": false
     }
   }`;
 
