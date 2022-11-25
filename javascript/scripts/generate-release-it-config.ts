ed6e574a13972e74af2e9d188cafa3ab9d352a29
Michel Haddad <michel@celitech.com>
2022-11-25 17:12:06 +0200

    Create scripts for config generation

---
+++ javascript/scripts/generate-release-it-config.ts
@@ -0,0 +1,73 @@
+// {
+//     "hooks": {},
+//     "git": {
+//       "changelog": "git log --pretty=format:\"* %s (%h)\" ${from}...${to}",
+//       "requireCleanWorkingDir": true,
+//       "requireBranch": false,
+//       "requireUpstream": true,
+//       "requireCommits": false,
+//       "addUntrackedFiles": false,
+//       "commit": true,
+//       "commitMessage": "Release ${version}",
+//       "commitArgs": [],
+//       "tag": true,
+//       "tagName": null,
+//       "tagMatch": null,
+//       "tagAnnotation": "Release ${version}",
+//       "tagArgs": [],
+//       "push": true,
+//       "pushArgs": ["--follow-tags"],
+//       "pushRepo": ""
+//     },
+//     "npm": {
+//       "publish": true,
+//       "publishPath": ".",
+//       "publishArgs": [],
+//       "tag": null,
+//       "otp": null,
+//       "ignoreVersion": false,
+//       "allowSameVersion": false,
+//       "versionArgs": [],
+//       "skipChecks": false,
+//       "timeout": 10
+//     },
+//     "github": {
+//       "release": false,
+//       "releaseName": "Release ${version}",
+//       "releaseNotes": null,
+//       "autoGenerate": false,
+//       "preRelease": false,
+//       "draft": false,
+//       "tokenRef": "GITHUB_TOKEN",
+//       "assets": null,
+//       "host": null,
+//       "timeout": 0,
+//       "proxy": null,
+//       "skipChecks": false,
+//       "web": false
+//     }
+//   }
+
+import * as fs from "fs";
+
+let GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
+
+const codeToGenerate = `{
+    "hooks": {},
+    "npm":{
+        "ignoreVersion": false,
+        "allowSameVersion": false
+    },
+    "github": {
+        "tokenRef": "${GITHUB_TOKEN}",
+        "release": true,
+        "autoGenerate": true
+    }
+  }`;
+
+fs.writeFile("release-it.json", codeToGenerate, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("config file was saved!");
+});
---
+++ javascript/scripts/generate-test-url-config.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+let OAUTH2_URL =
+  process.env["OAUTH2_URL"] ||
+  "https://test-core-partners.auth.us-east-1.amazoncognito.com/oauth2/token";
+let API_URL =
+  process.env["API_URL"] ||
+  "https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test";
+
+const codeToGenerate =
+  `export const OAUTH2_URL = "${OAUTH2_URL}";\n` +
+  `export const API_URL = "${API_URL}";\n`;
+
+if (!fs.existsSync("../src/config")) {
+  fs.mkdirSync("config");
+}
+
+fs.writeFile("../src/config/urls-config.ts", codeToGenerate, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("config file was saved!");
+});
