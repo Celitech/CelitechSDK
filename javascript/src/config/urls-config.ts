b817e7487bce1713a8f99fa96fc00d41e20105de
Michel Haddad <michel@celitech.com>
2022-11-28 20:17:08 +0200

    Remove scripts

---
+++ javascript/src/config/urls-config.ts
@@ -0,0 +1,5 @@
+export const TEST_OAUTH2_URL = "https://test-core-partners.auth.us-east-1.amazoncognito.com/oauth2/token";
+export const TEST_API_URL = "https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test";
+
+export const LIVE_OAUTH2_URL = "https://auth.celitech.net/oauth2/token";
+export const LIVE_API_URL = "https://api.celitech.net/v1";
