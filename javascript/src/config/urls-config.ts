24db697d049d2cbc78507b9e3a5a01a2336714ad
zgheibali <azgheib@celitech.com>
2025-06-03 07:41:05 +0300

    Update oauth urls

--- javascript/src/config/urls-config.ts
+++ javascript/src/config/urls-config.ts
@@ -1,8 +1,10 @@
-export const TEST_OAUTH2_URL = "https://d8qsxtw5yojys.cloudfront.net/oauth2/token";
+export const TEST_OAUTH2_URL =
+  "https://6oekcbcrie.execute-api.us-east-1.amazonaws.com/test/oauth2/token";
 export const TEST_API_URL =
   "https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test";
 
-export const QA_OAUTH2_URL = "https://d2vmyqu8k8z26a.cloudfront.net/oauth2/token";
+export const QA_OAUTH2_URL =
+  "https://zbfzjjtkp7.execute-api.us-east-1.amazonaws.com/qa/oauth2/token";
 export const QA_API_URL =
   "https://1bki1q3be6.execute-api.us-east-1.amazonaws.com/qa";
 
